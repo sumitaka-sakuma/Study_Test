11ed0dd1d530397339a36cbc6360cfb7d3ea8285
sumitaka-sakuma <tksn.playthe69gt2@gmail.com>
2021-01-23 16:18:40 +0900

    first commit

---
+++ src/sample/sample.java
@@ -0,0 +1,22 @@
+package sample;
+
+public class sample {
+
+	public static void main(String args[]){
+
+		int num1 = 2;
+		int num2 = 5;
+
+		int result = sum(num1, num2);
+
+		System.out.println(result);
+
+	}
+
+	public static int sum(int num1, int num2){
+
+		int result = num1 + num2;
+
+		return result;
+	}
+}

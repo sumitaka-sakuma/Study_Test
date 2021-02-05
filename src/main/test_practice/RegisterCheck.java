d457180f87417ff1555a9a3d08d883e461124fdf
sumitaka-sakuma <tksn.playthe69gt2@gmail.com>
2021-02-05 22:58:18 +0900

    Added RegisterCheck

---
+++ src/main/test_practice/RegisterCheck.java
@@ -0,0 +1,32 @@
+package test_practice;
+
+public class RegisterCheck {
+
+	public static void main(String[] args) {
+
+		int age = 0;
+		boolean isRegisterMailMagazine = false;
+		int userPastMonth = 0;
+
+		isSpecialMember(age, isRegisterMailMagazine, userPastMonth);
+
+	}
+
+	public static boolean isSpecialMember(int age, boolean isRegisterMailMagazine, int userPastMonth) {
+
+		if (age < 20) {
+			return false;
+		}
+
+		if (!isRegisterMailMagazine) {
+			return false;
+		}
+
+		if (userPastMonth < 1) {
+			return false;
+		}
+
+		return true;
+	}
+
+}

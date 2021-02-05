2f01b8681c250bd90d9a65cc622073c8a2c153a4
sumitaka-sakuma <tksn.playthe69gt2@gmail.com>
2021-02-05 23:17:42 +0900

    comment

--- src/main/test_practice/RegisterCheck.java
+++ src/main/test_practice/RegisterCheck.java
@@ -1,26 +1,36 @@
 package test_practice;
 
+/**
+ * 特別会員かを判定するクラス
+ * @author sakumasumitaka
+ *
+ */
 public class RegisterCheck {
 
 	public static void main(String[] args) {
 
+		// 年齢
 		int age = 0;
+		// メールマガジンに登録しているかのフラグ
 		boolean isRegisterMailMagazine = false;
+		// 前月の利用回数
 		int userPastMonth = 0;
 
+		// 特別会員かの判定メソッド
 		isSpecialMember(age, isRegisterMailMagazine, userPastMonth);
 	}
 
 	public static boolean isSpecialMember(int age, boolean isRegisterMailMagazine, int userPastMonth) {
 
+		// 20歳未満は特別会員でない
 		if (age < 20) {
 			return false;
 		}
-
+		// メールマガジンに登録してない場合、特別会員でない
 		if (!isRegisterMailMagazine) {
 			return false;
 		}
-
+		// 前月1回も利用してない場合、特別会員でない
 		if (userPastMonth < 1) {
 			return false;
 		}
